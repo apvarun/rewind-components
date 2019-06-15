0abad7d34f7a22735ce9fefa34a98e241f56816e
apvarun <varunlakshmananap@gmail.com>
2019-06-15 15:29:18 +0530

    Build Setup: added rollup build step
    - Rollup for Build
    - Jest for tests
    - Babel for transpilation
    + build optimizations

---
+++ components/index.js
@@ -0,0 +1,15 @@
+import Button from "./Button/index.jsx";
+import Card from "./Card/index.jsx";
+import Avatar from "./Avatar/index.jsx";
+import Chip from "./Chip/index.jsx";
+import List from "./List/index.jsx";
+import ListItem from "./List/ListItem.jsx";
+import Divider from "./Divider/index.jsx";
+import Radio from "./Radio/index.jsx";
+import NavBar from "./NavBar/index.jsx";
+import Alert from "./Alert/index.jsx";
+
+import "./style.css";
+
+export { Button, Card, Avatar, Alert, Chip, List, ListItem, Divider, Radio, NavBar };
+export default { Button, Card, Avatar, Alert, Chip, List, ListItem, Divider, Radio, NavBar };
